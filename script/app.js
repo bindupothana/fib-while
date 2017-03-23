ea7b596f9119187586c1b8b72ff987347257b7a3
bindupothana <himabindu.pothana@navaratan.com>
2017-03-23 18:03:02 +0530

    Fibonacci using while

--- script/app.js
+++ script/app.js
@@ -2,24 +2,31 @@ var app=angular.module('fibApp',[]);
 app.controller('fibCtrl',function($scope){
 	 
 	$scope.fibonacci=function(num){
-	 var i=1;
-	 var a=0, b=1 ,output=0; 
+	 var i=0;
+	 var a=0, b=1 ,output=1; 
 	 $scope.result = [];
    	 $scope.result.push(a)
 	 $scope.result.push(b)
-	 while(i < $scope.num-a){
-      console.log("Result", $scope.result)
-	    output=a+b;
 
+	 while(output<$scope.num){
+      console.log("Result", $scope.result)
+      $scope.result.push(output)
+	    output=a+b;
 	     a=b;
-	      b=output;	   
-	      ++i;   
-	     $scope.result.push(output)	        
-	        // i++;
+	     b=output;	     
+	   //if(output<num){
+
+	    		
+	   // }
+	   
+	    
+	    // i++;   
+
+	      
 
 	  }
-	  // i++;
-	    return a;
+	  
+	    return output;
        console.log("Result", $scope.result)
 
 	}
